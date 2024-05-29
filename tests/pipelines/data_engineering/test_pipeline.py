16e85a0f143560268157bc8971416ac96a9f0bce
julianafalves <julianaferreira@estudante.ufscar.br>
2024-05-29 19:20:02 -0300

    add simple test

--- tests/pipelines/data_engineering/test_pipeline.py
+++ tests/pipelines/data_engineering/test_pipeline.py
@@ -7,3 +7,25 @@ Kedro recommends using `pytest` framework, more info about it can be found
 in the official documentation:
 https://docs.pytest.org/en/latest/getting-started.html
 """
+# test_data_processing.py
+import pandas as pd
+from kedro_datasets.pandas import CSVDataset
+
+from pathlib import Path
+
+def test_diagnoses_values():
+    # Load the dataset
+    data_catalog = {
+        "phenotypic_values": CSVDataset(filepath="data/01_raw/phenotypesNumeric.csv")
+    }
+    phenotypic_data = data_catalog["phenotypic_values"].load()
+
+    # Check if 'diagnoses' column exists
+    assert "Diag" in phenotypic_data.columns, "Column 'diagnoses' not found in the dataset."
+
+    # Extract the 'diagnoses' column
+    diagnoses_column = phenotypic_data['Diag']
+
+    # Verify that all values in 'diagnoses' column are 1, 2, or 3
+    assert all(diagnoses_column.isin([1, 2, 3])), "Invalid values found in 'diagnoses' column."
+
