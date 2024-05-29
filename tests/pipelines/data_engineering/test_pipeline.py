baaa788e6ada51144e992aa2df6bdc6a5647a0b9
julianafalves <julianaferreira@estudante.ufscar.br>
2024-05-29 19:22:13 -0300

    add simple test without boilerplate comment

--- tests/pipelines/data_engineering/test_pipeline.py
+++ tests/pipelines/data_engineering/test_pipeline.py
@@ -1,12 +1,3 @@
-"""
-This is a boilerplate test file for pipeline 'data_engineering'
-generated using Kedro 0.19.5.
-Please add your pipeline tests here.
-
-Kedro recommends using `pytest` framework, more info about it can be found
-in the official documentation:
-https://docs.pytest.org/en/latest/getting-started.html
-"""
 # test_data_processing.py
 import pandas as pd
 from kedro_datasets.pandas import CSVDataset
